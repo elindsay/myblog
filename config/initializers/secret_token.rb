0e418b02a46b00df4cb88d8513d11f8e4061369f
Emma Lindsay <emma@flickme.com>
2013-01-09 20:44:32 -0800

    Created repository

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Myblog::Application.config.secret_token = '14040c278cafac474290a6cd962bd1ecc850faf34089cf3729985b467d44223908cccee9934f0d3c4b2761488b3c7e61068a3cef05131ba138006bb1c6ff0a76'
